500900a876a067c76dc87146fd634931b7f3a90b
Amara Datta <amaradatta@gmail.com>
2018-09-18 23:43:27 -0700

    restructure the views and util

--- reports/views.py
+++ reports/views.py
@@ -1,27 +1,8 @@
 from django.shortcuts import render
 
-from .utils import get_device_info, date_and_time, get_account_name
+from .utils import return_device_info
 
 
-def all_device_info(request, account_key):
-    device_info = get_device_info(account_key)
-    name = get_account_name(account_key)
-    output = {
-        'Account': name,
-        'Device': []
-    }
-    for device in device_info:
-        date_time = date_and_time(device.imei)
-        device_date = date_time[0]
-        device_time = date_time[1]
-        output['Device'].append({
-            'IMEI': device.imei,
-            'SIM_Number': device.sim_no,
-            'Added_On': device.added_on,
-            'Last_Reported_Date': device_date,
-            'Last_Reported_Time': device_time,
-            'Device_Status': device.device_status,
-            'Billing_Status': device.billing_status,
-            'Comments': device.comments.replace("\r\n", " "),
-        })
+def all_device_info(request, account_key=None):
+    output = return_device_info(account_key)
     return render(request, 'dashboard.html', {'output': output})
