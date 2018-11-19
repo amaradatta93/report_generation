71947926221514ccb1f65d5778240307e28937f6
Amara Datta <amaradatta@gmail.com>
2018-11-18 22:16:08 -0800

    add form to take number of days

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,4 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.show_all_accounts, name='account_name'),
     path('all', views.not_reported_for_two_days, name='unresponsive'),
-    # path('account_info', views.all_device_and_billing_status, name='device_and_billing_status'),
-    # path('all_account_info', views.all_device_and_billing_status, name='device_and_billing_status'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 
 from .utils import get_all_devices_for_account
 
@@ -11,3 +13,18 @@ def not_reported_for_two_days(request, account_key):
     '''
     response = get_all_devices_for_account(account_key)
     return render(request, 'dashboard.html', {'response': response})
+
+
+def get_not_reporting_time():
+    pass
+
+
+def days_html(request):
+    return render(request, 'get_days.html', content_type='text/html')
+
+
+def get_number_of_days(request):
+    if request.method == 'POST':
+        data_form = request.POST
+        days = data_form['numberOfDays']
+        return HttpResponse(days)
