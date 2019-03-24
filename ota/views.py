3fd6c3a5a0e4e83fd71fee7909a6a71b8b8cabbd
Amara Datta <amaradatta@gmail.com>
2019-03-24 15:22:15 -0700

    add utils to handle the reset command for each device

--- ota/views.py
+++ ota/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 
+from .reset_utils import reset_command
+
 
 def reset_device(request):
     response = {
         'imei_number': request.GET.get('imei_number'),
         'device_name': request.GET.get('device_name'),
-        'command': 'reset'
+        'command': reset_command(request.GET.get('imei_number'), request.GET.get('device_name'))
     }
 
     return HttpResponse(response.values())
@@ -15,20 +17,15 @@ def ping_device(request):
     response = {
         'imei_number': request.GET.get('imei_number'),
         'device_name': request.GET.get('device_name'),
-        'command': 'pinging'
     }
 
-    if response['device_name'] == 'SteppIII_JSPR_US':
-        return HttpResponse(response.values())
-    else:
-        return HttpResponse('Ntothing')
+    return HttpResponse(response.values())
 
 
 def set_device_apn(request):
     response = {
         'imei_number': request.GET.get('imei_number'),
         'device_name': request.GET.get('device_name'),
-        'command': 'set apn'
     }
 
     return HttpResponse(response.values())
