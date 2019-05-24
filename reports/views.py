6f2363dd5e19064a7a34298f7e6823b581c1f12e
Eliyaz <eliyaz@qwickbit.com>
2019-05-24 14:54:15 -0700

    order devices by date

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,5 @@
+from _datetime import datetime
+
 from django.shortcuts import render
 
 from .utils import get_all_devices_for_account
@@ -10,8 +12,7 @@ def not_reported_for_two_days(request, account_key):
     :return: list of devices as dictionary
     '''
     days = request.session['temp_data']
-#    request.session['Account_Id'] = account_key
- #   print('-----------------------------------------------------------')
-  #  print(request.session['Account_Id'])
     response = get_all_devices_for_account(account_key, days)
+    response[0]['account_devices'].sort(key=lambda x: datetime.strptime(x['last_reported_date'], '%d.%m.%Y'), reverse=True)
     return render(request, 'account_devices.html', {'response': response})
+
